44920af70a27752dbf39bdae4ae9b09cfb928798
sebastianpena321 <sebatinapena950@gamilcom>
2024-03-18 03:11:25 -0500

    add: moto1

--- reserva.php
+++ reserva.php
@@ -16,6 +16,8 @@ session_start();
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     <link rel="stylesheet" type="text/css"
         href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+
+    <link rel="stylesheet" href="Estilos/slick-theme.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script src="javaScript/slider.js"></script>
