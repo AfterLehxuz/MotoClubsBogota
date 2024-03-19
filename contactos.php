0a2ab767773da501b8027de6d875e527d6155d5a
sebastianpena321 <sebatinapena950@gamilcom>
2024-03-19 05:01:59 -0500

    contactos

--- contactos.php
+++ contactos.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,25 +8,21 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap' rel='stylesheet'>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
-
     <link rel="stylesheet" href="Estilos/contactos.css">
     <link rel="shortcut icon" href="Estilos/imagenes/logo.png" type="image/x-icon">
     <link rel="icon" type="image/x-icon" href="Imagenes/Logo(1).ico">
     <title>Contactos</title>
-    <style>
-    .logo img {
-        border-radius: 10%; /* Esto hace que la imagen sea redonda */
-    }
-</style>
+    
 </head>
 
 <body>
     <header>
-    <a href="index.php" class="logo"> 
-        <img src="Imagenes/Logo.png" alt="Icono de la empresa">Moto Clubs Bogota
-    </a>
+        <a href="index.php" class="logo">
+            <img src="Imagenes/Logo.png" alt="Icono de la empresa">Moto Clubs Bogota
+        </a>
         <nav>
             <ul>
                 <li id="inicio"><a href="index.php">Inicio</a></li>
@@ -43,28 +38,42 @@ session_start();
                 echo '<span><i class="bx bx-user"></i></span>';
                 echo '<span class="nombre-usuario">' . $_SESSION["nombre"] . '</span>';
                 echo '</div>';
-                echo '<a href="logout.php"><button>Cerrar sesión</button></a>';
-                echo '<a href="dashboard.php"><button>Perfil</button></a>'; // Agrega este enlace para ir a dashboard.php
+                echo '<a href="logout.php"><button id="cerrar-sesion">Cerrar sesión</button></a>';
+                echo '<a href="dashboard.php"><button id="perfil">Perfil</button></a>'; // Agrega este enlace para ir a dashboard.php
             } else {
                 // El usuario no ha iniciado sesión, muestra los botones "Iniciar sesión" y "Registrarse"
-                echo '<a href="login.php"><button>Iniciar sesión</button></a>';
-                echo '<a href="signup.php"><button>Registrarse</button></a>';
+                echo '<a href="login.php"><button id="iniciar-sesion">Iniciar sesión</button></a>';
+                echo '<a href="signup.php"><button id="registrarse">Registrarse</button></a>';
             }
-
             ?>
         </nav>
     </header>
     <section id="contactos">
         <h2>Contáctanos</h2>
-        <p>Si tienes alguna pregunta o quieres comunicarte con nosotros, puedes hacerlo a través de los siguientes
-            medios:</p>
+        <p>Si tienes alguna pregunta o quieres comunicarte con nosotros, puedes hacerlo a través de los siguientes medios:</p>
+
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <img src="Imagenes/moto4.jpg" alt="Imagen 1">
+                </div>
+                <div class="swiper-slide">
+                    <img src="ruta_a_tu_imagen_2.jpg" alt="Imagen 2">
+                </div>
+                <!-- Agrega más imágenes si es necesario -->
+            </div>
+            <!-- Agrega la paginación y los botones de navegación -->
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-next"></div>
+            <div class="swiper-button-prev"></div>
+        </div>
+
         <div class="info-contacto">
             <div class="item">
-
-                <i class= 'bx bxl-facebook' style="color: black;"></i>
-                
+                <i class='bx bxl-facebook' style="color: black;"></i>
                 <p> <a href="https://www.facebook.com/Motoclubsbogota/" style="color: black; font-weight: bold;">Moto cluB'S</a></p>
             </div>
+
 
             <div class="item">
                 <i class='bx bxl-whatsapp' style="color: black;"></i>
@@ -72,24 +81,10 @@ session_start();
             </div>
 
             <div class="item">
-                <i class='bx bx-map' style="color: black;"></i>
-                <p> <a href="https://www.bing.com/search?pglt=41&q=78h19+Av+Cd+de+Villavicencio%2C+Bogot%C3%A1%2C+Colombia&cvid=0a74a846a5504190a8b3cc238b056ec9&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBBzM5NGowajGoAgCwAgA&FORM=ANNTA1&adppc=EDGEESS&PC=U531" style="color: black; font-weight: bold; "> 78h19 Av Cd de Villavicencio, Bogotá, Colombia</a></p> </p>
-            </div>
-
+                <i class='bx bx-map'></i>
+                <p><a href="https://www.bing.com/search?pglt=41&q=78h19+Av+Cd+de+Villavicencio%2C+Bogot%C3%A1%2C+Colombia&cvid=0a74a846a5504190a8b3cc238b056ec9&gs_lcrp=EgZjaHJvbWUyBggAEEUYOdIBBzM5NGowajGoAgCwAgA&FORM=ANNTA1&adppc=EDGEESS&PC=U531" style="color: black; font-weight: bold; "> 78h19 Av Cd de Villavicencio, Bogotá, Colombia</a></p>
         </div>
-
-        <form action="enviar.php" method="post">
-            <h3>Formulario de contacto</h3>
-            <input type="text" name="nombre" placeholder="Tu nombre" required>
-            <input type="email" name="correo" placeholder="Tu correo electrónico" required>
-            <textarea name="mensaje" placeholder="Tu mensaje" required></textarea>
-            <button type="submit"" >Enviar mensaje</button>
-        </form>
     </section>
-
-
-    <footer>
-    </footer>
 </body>
 
 </html>
